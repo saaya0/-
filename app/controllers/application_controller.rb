e66f04b8546b44c81af0d78fe645c08213fba54a
saaya0 <sayaaa04155@gmail.com>
2022-01-10 12:20:26 +0000

    fix syntax error: application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  #before_action :set_header
   def top; end
+
+
+  private
+
+  def set_header
+    @is_header = true
+    paths = [root_path, new_user_session_path, new_user_registration_path, new_admin_session_path]
+    paths.each do |path|
+      if current_page?(path)
+        @is_header = false
+      end
+    end
+  end
+
 end

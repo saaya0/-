b2562d5437729962216e284eb4b9556d2b025bd4
saaya0 <sayaaa04155@gmail.com>
2022-01-10 12:32:13 +0000

    add set_header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
   def set_header
-    @is_header = true
-    paths = [root_path, new_user_session_path, new_user_registration_path, new_admin_session_path]
-    paths.each do |path|
-      if current_page?(path)
-        @is_header = false
-      end
-    end
+    @header = true
+    @paths = [root_path, new_user_session_path, new_user_registration_path, new_admin_session_path]
   end
 
 end
